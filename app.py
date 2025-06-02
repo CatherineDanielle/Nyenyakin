53f1268af470a28d31f51ddfeffba461c9cd95a1
cdanielle <96690358+CatherineDanielle@users.noreply.github.com>
2025-06-02 14:06:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,11 +51,6 @@ def predict():
     prediction = model.predict(input_features)[0]
     return jsonify({'prediction': str(prediction)})
 
-def handler(environ, start_response): 
-    return DispatcherMiddleware(Flask('dummy'), {
-        "/api": app
-    })(environ, start_response)
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
