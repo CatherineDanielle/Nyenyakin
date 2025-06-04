ddbdf0023b7109893b89711939d5393b04ab8b32
cdanielle <96690358+CatherineDanielle@users.noreply.github.com>
2025-06-04 21:44:41 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Test from './pages/Test';
 import Tips from './pages/Tips';
+import Good from './pages/Goodpage';
+import Bad from './pages/Badpage'; 
 
 export default function App() {
   return (
@@ -14,6 +16,8 @@ export default function App() {
         <Route path="/test" element={<Test />} />
         <Route path="/tips" element={<Tips />} />
         <Route path="/tips/:articleId" element={<Tips />} />
+        <Route path="/good" element={<Good />} /> 
+        <Route path="/bad" element={<Bad />} /> 
         <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
